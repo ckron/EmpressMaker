755c65d4d844da7f5e69e7635699b6658ec01af0
ckron <7393106+ckron@users.noreply.github.com>
2020-05-20 15:30:14 +0900

    add function of select connecting text

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-function OnButtonClick(obj) {
-    if(obj.id == "create_text") {
+function OnButtonClick(e) {
+    if(e.id == "create_text") {
         let inText = document.getElementById("in_text");
         let outText = document.getElementById("out_text");
+        let connectText = document.getElementById("select_pulldown").value;
 
-        outText.value = inText.value + "だからこそ、" + inText.value;
-    } else if(obj.id == "copy_clipboard") {
+        outText.value = inText.value + connectText + inText.value;
+    } else if(e.id == "copy_clipboard") {
         let copyTextArea = document.getElementById("out_text");
 
         copyTextArea.select();
         document.execCommand("copy");
+    }
+}
+
+function OnChange(e) {
+    if(e.id == "select_pulldown") {
+        let inText = document.getElementById("in_text");
+        let outText = document.getElementById("out_text");
+        let connectText = document.getElementById("select_pulldown").value;
+
+        outText.value = inText.value + connectText + inText.value;
     }
 }
