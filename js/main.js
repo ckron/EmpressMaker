f002d74b7af2faf873ad463bb051605cb272b133
ckron <7393106+ckron@users.noreply.github.com>
2020-05-20 15:06:56 +0900

    add function of copy clipboard

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-function OnButtonClick() {
-    let inText = document.getElementById("in_text");
-    let outText = document.getElementById("out_text");
+function OnButtonClick(obj) {
+    if(obj.id == "create_text") {
+        let inText = document.getElementById("in_text");
+        let outText = document.getElementById("out_text");
 
-    outText.value = inText.value + "だからこそ、" + inText.value;
+        outText.value = inText.value + "だからこそ、" + inText.value;
+    } else if(obj.id == "copy_clipboard") {
+        let copyTextArea = document.getElementById("out_text");
+
+        copyTextArea.select();
+        document.execCommand("copy");
+    }
 }
